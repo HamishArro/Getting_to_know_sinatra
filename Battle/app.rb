b591ba3785e2e6149c16f8eb8ea216c1fbc1f853
Hamish Arro <Hamisharro@icloud.com>
2021-01-20 17:27:39 +0000

    set RSPEC test for HP

--- Battle/app.rb
+++ Battle/app.rb
@@ -14,13 +14,17 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
+    session[:player_1_hp] = 100
+    session[:player_2_hp] = 100
     redirect '/play'
   end
 
   get '/play' do
     @player_1 = session[:player_1]
-    @player_2 = session[:player_2] 
-  erb(:play)
+    @player_2 = session[:player_2]
+    @player_1 = session[:player_1_hp]
+    @player_2 = session[:player_2_hp]
+    erb(:play)
   end
 
 end
--- Battle/spec/features/testing_infrastructure_spec.rb
+++ Battle/spec/features/testing_infrastructure_spec.rb
@@ -8,4 +8,12 @@ feature 'Testing infastructure' do
     expect(page).to have_content 'Hamish vs. Salar'
   end
 
+  scenario 'players can input their names' do
+    visit('/')
+    fill_in('Player 1', with: 'Hamish')
+    fill_in('Player 2', with: 'Salar')
+    click_button 'Submit'
+    expect(page).to have_content "Salar's HP: 100"
+  end
+
 end
