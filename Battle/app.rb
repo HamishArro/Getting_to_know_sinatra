5f6528bd814b99e7a475ec0dc18a1a62c7142500
Hamish Arro <Hamisharro@icloud.com>
2021-01-20 09:58:29 +0000

    add reloader to app.rb

--- Battle/app.rb
+++ Battle/app.rb
@@ -1,8 +1,18 @@
 require 'sinatra/base'
+require "sinatra/reloader"
 
 class Battle < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
 
   get '/' do
     'Testing infrastructure working!'
   end
+
+  get '/cat' do
+    'cat is not as good as dog 100%'
+  end
+
 end
